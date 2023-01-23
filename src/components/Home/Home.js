0a9fcab008857109cf25aad92d5a7181576b474e
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-23 09:54:39 +0300

    Overview section complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero';
+import Overview from './Overview';
 
 const Home = () => {
   return (
     <div className='home'>
       <Hero />
+      <Overview />
     </div>
   );
 };
---
+++ src/components/Home/Overview.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import donationImg from './../../assets/images/donation.jpg';
+import donorImg from './../../assets/images/donor.jpg';
+import eventImg from './../../assets/images/event-illustration.png';
+import illustrationImg from './../../assets/images/islam-illustration.webp';
+
+const Overview = () => {
+  return (
+    <section className='overview'>
+      {/* <div className='overview-item'>
+        <img src={donationImg} alt='donation' />
+        <div className='overview-details'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati, nemo rerum. Tempora mollitia distinctio eum
+            ipsa saepe provident aut nostrum.
+          </p>
+          <button>learn more</button>
+        </div>
+      </div> */}
+      <div className='overview-item'>
+        <img src={illustrationImg} alt='Islam illustration' />
+        <div className='overview-details'>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati, nemo rerum. Tempora mollitia distinctio eum
+            ipsa saepe provident aut nostrum.
+          </h4>
+          <button>learn more</button>
+        </div>
+      </div>
+      <div className='overview-item'>
+        <img src={eventImg} alt='event illustration' />
+        <div className='overview-details'>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati, nemo rerum. Tempora mollitia distinctio eum
+            ipsa saepe provident aut nostrum.
+          </h4>
+          <button>learn more</button>
+        </div>
+      </div>
+      <div className='overview-item'>
+        <img src={donorImg} alt='donor' />
+        <div className='overview-details'>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati, nemo rerum. Tempora mollitia distinctio eum
+            ipsa saepe provident aut nostrum.
+          </h4>
+          <button>learn more</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Overview;
