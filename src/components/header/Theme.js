3515052c071af22ca4e36166cb269208246bf46c
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-23 07:57:50 +0300

    Cleanup

--- src/components/header/Theme.js
+++ src/components/header/Theme.js
@@ -1,21 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   MdOutlineDarkMode,
   MdOutlineLightMode,
 } from 'react-icons/md';
 
 const Theme = ({ toggleTheme, theme }) => {
-  //   const [theme, setTheme] = useState(true);
-
-  // const toggleTheme = () => {
-  //   moveThemeSwitch();
-  //   //TODO Change theme
-  // };
 
   const moveThemeSwitch = () => {
     if (theme === 'light') return '0';
     return '50%';
   };
+  
   return (
     <div className='theme-icons' onClick={toggleTheme}>
       <MdOutlineDarkMode className='theme-icon' />
